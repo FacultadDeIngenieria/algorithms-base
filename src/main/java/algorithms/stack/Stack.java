5c6b0b0f7aba84de1602c9ccc6a65eaacaed695b
Leandro Bauret <leandro.bauret@mulesoft.com>
2023-03-23 15:24:54 -0300

    Initial commit

---
+++ src/main/java/algorithms/stack/Stack.java
@@ -0,0 +1,46 @@
+package algorithms.stack;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Algoritmos.
+ *
+ * Basic Interface for all {@link Stack stacks}. All Stack implementations should implement {@link Stack}.
+ *
+ * Common implementations:
+ *
+ * <ul>
+ *   <li>ArrayStack</li>
+ *   <li>LinkedListStack</li>
+ * </ul>
+ */
+public interface Stack<E> {
+    //~ Methods ..................................................................................................................
+    /**
+     * Adds a new item to the Stack
+     *
+     * @param  item to be pushed in the stack
+     */
+    void push(@NotNull final E item);
+
+    /**
+     * Remove and return the item most recently added
+     *
+     * @return  item
+     */
+    E pop();
+
+    /**
+     * Returns true if the stack has no items
+     *
+     * @return  true or false
+     */
+    boolean isEmpty();
+
+    /**
+     * Returns the number of items in the stack
+     *
+     * @return  size
+     */
+    int size();
+}
