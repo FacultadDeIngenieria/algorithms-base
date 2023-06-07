ab01b678ecc69cb635a08e122d691bd6466fb4cb
Leandro Bauret <leandro.bauret@mulesoft.com>
2023-06-06 23:54:16 -0300

    Add color to Node for TP7

--- src/main/java/algorithms/tree/Node.java
+++ src/main/java/algorithms/tree/Node.java
@@ -2,11 +2,18 @@ package algorithms.tree;
 
 public class Node<K, V> {
     K key;                      // sorted by key
-    V value;                      // associated data
+    V value;                    // associated data
     Node<K, V> left, right;     // left and right subtrees
+    boolean color;              // true for RED and false for BLACK
 
     public Node(K key, V val) {
         this.key = key;
         this.value = val;
+    }
+
+    public Node(K key, V val, boolean color) {
+        this.key = key;
+        this.value = val;
+        this.color = color;
     }
 }
